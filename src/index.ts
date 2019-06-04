79f18657fe68e8323a843cba0e26561aa3a8ad76
UnsignedInt8 <unsignedint8@gmail.com>
2019-06-04 14:24:37 +0800

    check running mode

--- src/index.ts
+++ src/index.ts
@@ -6,34 +6,37 @@ import Bot, { BotOptions } from './Bot';
 import Logger from './lib/Logger';
 import inquirer from 'inquirer';
 
-let program = commander
-    .option('-c, --config [path]', 'Configruation File Path', String)
-    .parse(process.argv);
+// Called directly
+if (require.main === module) {
+    let program = commander
+        .option('-c, --config [path]', 'Configruation File Path', String)
+        .parse(process.argv);
 
-let bot: Bot;
+    let bot: Bot;
 
-if (program.config) {
-    let json = fs.readFileSync(program.config, { encoding: 'utf8' });
-    let config = JSON.parse(json);
-    bot = new Bot(config);
-    bot.launch();
-} else {
-    (async () => {
-        try {
-            let { token } = await inquirer.prompt({ name: 'token', message: 'Bot Token:', type: 'input', }) as { token: string };
-            bot = new Bot({ token: token.trim() });
-            bot.launch();
-        } catch (error) {
-            Logger.error(error.message);
-            process.exit(1);
-        }
-    })();
+    if (program.config) {
+        let json = fs.readFileSync(program.config, { encoding: 'utf8' });
+        let config = JSON.parse(json);
+        bot = new Bot(config);
+        bot.launch();
+    } else {
+        (async () => {
+            try {
+                let { token } = await inquirer.prompt({ name: 'token', message: 'Bot Token:', type: 'input', }) as { token: string };
+                bot = new Bot({ token: token.trim() });
+                bot.launch();
+            } catch (error) {
+                Logger.error(error.message);
+                process.exit(1);
+            }
+        })();
+    }
+
+    process.on('SIGINT', async () => {
+        await bot.exit();
+        Logger.info('all users exited');
+        process.exit();
+    });
 }
-
-process.on('SIGINT', async () => {
-    await bot.exit();
-    Logger.info('all users exited');
-    process.exit();
-});
 
 export { Bot, Logger, BotOptions };
