8ce2558335b22cd0002daec268231778717095a3
UnsignedInt8 <unsignedint8@gmail.com>
2019-06-15 21:41:20 +0800

    handle puppet error

--- src/Bot.ts
+++ src/Bot.ts
@@ -166,6 +166,11 @@ export default class Bot {
             await deleteWechat();
         });
 
+        wechat.puppet.on('error', async () => {
+            ctx.reply(lang.message.error);
+            await deleteWechat();
+        });
+
         wechat.on('message', msg => this.handleWechatMessage(msg, ctx));
 
         await wechat.start();
