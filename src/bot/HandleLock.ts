4b9ebce6d0f2bb71a6dbaf280aa9d44469f1e16d
UnsignedInt8 <unsignedint8@gmail.com>
2020-06-16 10:37:42 +0800

    fix empty user name

--- src/bot/HandleLock.ts
+++ src/bot/HandleLock.ts
@@ -1,12 +1,20 @@
 import { TelegrafContext } from 'telegraf/typings/context';
 import { Client } from '../Bot';
 import lang from '../strings';
-import { Contact } from 'wechaty';
+import { Contact, Room } from 'wechaty';
 
 export default async (ctx: TelegrafContext) => {
   let user = ctx['user'] as Client;
   if (!user.currentContact) return;
   if (user.contactLocked) return;
   user.contactLocked = true;
-  ctx.reply(lang.message.contactLocked((user.currentContact as Contact).name()));
+
+  let name = '';
+  if (user.currentContact instanceof Contact) {
+    name = user.currentContact.name();
+  } else if (user.currentContact instanceof Room) {
+    name = await user.currentContact.topic();
+  }
+
+  await ctx.reply(lang.message.contactLocked(name));
 };
