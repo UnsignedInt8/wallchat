413361bb6a278f98e639d909cd7da2f29c1994f9
UnsignedInt8 <unsignedint8@gmail.com>
2020-07-04 10:33:46 +0800

    fix filepath

--- src/bot/UpdateTmpFile.ts
+++ src/bot/UpdateTmpFile.ts
@@ -1,18 +1,24 @@
 import fs from 'fs';
+import tmpDir from 'temp-dir';
+import path from 'path';
 
 interface ITmpFile {
   recentContact?: { name: string; locked?: boolean };
 }
 
-export function writeFile(path: string, content: ITmpFile) {
+export function writeFile(filename: string, content: ITmpFile) {
+  const filepath = path.join(tmpDir, filename);
+
   return new Promise(resolve => {
-    fs.writeFile(path, JSON.stringify(content), { encoding: 'utf8' }, () => resolve());
+    fs.writeFile(filepath, JSON.stringify(content), { encoding: 'utf8' }, () => resolve());
   });
 }
 
-export function readFile(path: string) {
+export function readFile(filename: string) {
+  const filepath = path.join(tmpDir, filename);
+
   return new Promise<ITmpFile>(resolve => {
-    fs.readFile(path, { encoding: 'utf8' }, (err, data) => {
+    fs.readFile(filepath, { encoding: 'utf8' }, (err, data) => {
       let content: ITmpFile = {};
 
       try {
